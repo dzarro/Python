4c82f701d0b840a127ee13d2762c0830fd19a756
dzarro <dmzarro@gmail.com>
2025-10-19 13:39:25 -0400

    Sunday update

--- project.py
+++ project.py
@@ -9,18 +9,29 @@ def plot():
 	nu=np.array([7,7.3,7.7,8,8.3,8.7,9,9.3,9.7,10])
 	Fnu=np.array([-21.88,-21.55,-21.67,-21.86,-22.09,-22.38,-22.63,-22.96,-23.43,-23.79])
 	
+# Compute linear least squares fit of power law
+
+	coeff=np.polyfit(nu[1:],Fnu[1:],1)
+	index=coeff[0]
+	print(f"\nCygnus radio spectral index = {index:.2f}")
+	power=index*nu+coeff[1]
+	
 # Plot data
 
-	plt.plot(nu,Fnu)
-	plt.ylabel("Log10 Fnu (W m-2 Hz-1)")
-	plt.xlabel("Log10 Nu (Hz)")
+	plt.clf()
+	plt.scatter(nu,Fnu)
+	plt.plot(nu,Fnu,color='red',label='Measured')
+	plt.plot(nu,power,color='blue',label='Fitted')
+	plt.ylabel('log'+r'$_{10}\ F_\nu$'+' (W m-2 Hz-1)')
+	plt.xlabel('log'+r'$_{10}\ \nu$'+' (Hz)')
+	plt.legend()
 	plt.show(block=False)
 	
-# Compute area using trapezoidal rule on frequency vs monoschromatic flux)
+# Compute area using trapezoidal rule on frequency vs monoschromatic flux
 # This yields total flux in watts per meter squared
 
 	area=np.trapz(10**Fnu,10**nu)
-	print(f"\nCygnus flux = {area:.2e} Watts m-2")
+	print(f"Cygnus flux = {area:.2e} Watts m-2")
 	
 # Compute luminosity from flux x 4 pi x distance squared
 # Must convert parsecs to meters
@@ -30,7 +41,8 @@ def plot():
 	distance=cygnus_parsecs*meters_per_parsec
 	print(f"Cygnus distance = {distance:.2e} meters")
 	luminosity=area*4*math.pi*distance**2
-	print(f"Cygnus luminosity = {luminosity:.2e} Watts ")
+	ergs=luminosity*10**7
+	print(f"Cygnus radio luminosity = {luminosity:.2e} Watts or {ergs:.2e} ergs/s")
 		
 if __name__ == "__main__":
     plot()	
